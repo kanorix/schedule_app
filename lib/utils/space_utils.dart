62a64c51d78861064bcc42e4629ba5818b845a91
u9uru <u9uru@users.noreply.github.com>
2020-10-18 02:14:05 +0000

    deploy: 193edc4716ea52a4b11a509496cd0fbd4759c802

--- lib/utils/space_utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class SpaceUtils {
-  static const normal = EdgeInsets.all(16);
-  static const half = EdgeInsets.all(8);
-}
--- lib/utils/validator.dart
+++
@@ -1,9 +0,0 @@
-class Validator {
-  Validator._();
-
-  static bool isDate(String value) {
-    const _regExpString = r'^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$';
-    return value.isNotEmpty &&
-        RegExp(_regExpString, caseSensitive: true).hasMatch(value);
-  }
-}
